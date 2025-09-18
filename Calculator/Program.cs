76cf57669459c32c05ebacc44366f962bea52377
NikolaZnixy <nikolahegl@gmail.com>
2025-09-18 08:46:05 +0200

    Adding operations

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class CalculatorV2
+{
+    static void Main()
+    {
+        Console.WriteLine("Simple Calculator V2 - Multiple Operations");
+        Console.Write("Enter first number: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter an operator (+, -, *, /): ");
+        char op = Console.ReadKey().KeyChar;
+        Console.WriteLine();
+
+        Console.Write("Enter second number: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        double result = 0;
+        switch (op)
+        {
+            case '+': result = num1 + num2; break;
+            case '-': result = num1 - num2; break;
+            case '*': result = num1 * num2; break;
+            case '/': result = num2 != 0 ? num1 / num2 : double.NaN; break;
+            default:
+                Console.WriteLine("Invalid operator.");
+                return;
+        }
+
+        Console.WriteLine($"Result: {result}");
+    }
+}
